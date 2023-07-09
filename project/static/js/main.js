4e27b2e1735045fdc879859dbb6c753502b4a1f8
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-07-09 11:23:56 +0200

    Send Message function

--- project/static/js/main.js
+++ project/static/js/main.js
@@ -53,6 +53,83 @@ function getCookie(name) {
     return cookieValue
 }
 
+function sendMessage() {
+    chatSocket.send(JSON.stringify({
+        'type': 'message',
+        'message': chatInputElement.value,
+        'name': chatName
+    }))
+
+    chatInputElement.value = ''
+}
+
+
+function onChatMessage(data) {
+    console.log('onChatMessage', data)
+
+    if (data.type == 'chat_message') {
+        let tmpInfo = document.querySelector('.tmp-info')
+
+        if (tmpInfo) {
+            tmpInfo.remove()
+        }
+        
+        if (data.agent) {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+
+                    <div>
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+                        
+                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+                    </div>
+                </div>
+            `
+        } else {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                    <div>
+                        <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+                        
+                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+                    </div>
+
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+                </div>
+            `
+        }
+    } else if (data.type == 'users_update') {
+        chatLogElement.innerHTML += '<p class="mt-2">The admin/agent has joined the chat!'
+    } else if (data.type == 'writing_active') {
+        if (data.agent) {
+            let tmpInfo = document.querySelector('.tmp-info')
+
+            if (tmpInfo) {
+                tmpInfo.remove()
+            }
+
+            chatLogElement.innerHTML += `
+                <div class="tmp-info flex w-full mt-2 space-x-3 max-w-md">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+
+                    <div>
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">The agent/admin is writing a message</p>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+
+    scrollToBottom()
+}
+
 
 
 async function joinChatRoom() {
