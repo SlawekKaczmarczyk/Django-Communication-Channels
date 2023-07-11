e9fbfe271c7550db65616b044e7bf976e8dbac2b
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-07-11 15:52:31 +0200

    sending/receiving messages

--- project/chat/consumers.py
+++ project/chat/consumers.py
@@ -3,6 +3,13 @@ import json
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+from django.utils.timesince import timesince
+
+from account.models import User
+
+from .models import Room, Message
+from .templatetags.chatextras import initials
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -13,6 +20,59 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name,self.channel_name)
         await self.accept()
 
+
     async def disconnect(self, close_code):
         #Leave room
-         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        
+        if not self.user.is_staff:
+            await self.set_room_closed()
+        
+    async def receive(self, text_data):
+        # Receive message from WebSocket (front end)
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+        message = text_data_json['message']
+        name = text_data_json['name']
+        agent = text_data_json.get('agent', '')
+
+        print('Receive:', type)
+
+        if type == 'message':
+            new_message = await self.create_message(name, message, agent)
+
+            # Send message to group / room
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'chat_message',
+                    'message': message,
+                    'name': name,
+                    'agent': agent,
+                    'initials': initials(name),
+                    'created_at': timesince(new_message.created_at),
+                }
+            )
+        elif type == 'update':
+            print('is update')
+            # Send update to the room
+            await self.channel_layer.group_send(
+                self.room_group_name, {
+                    'type': 'writing_active',
+                    'message': message,
+                    'name': name,
+                    'agent': agent,
+                    'initials': initials(name),
+                }
+            )
+
+    async def chat_message(self, event):
+        # Send message to WebSocket (front end)
+        await self.send(text_data=json.dumps({
+            'type': event['type'],
+            'message': event['message'],
+            'name': event['name'],
+            'agent': event['agent'],
+            'initials': event['initials'],
+            'created_at': event['created_at'],
+        }))
+
